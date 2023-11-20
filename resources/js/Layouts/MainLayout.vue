3da9905f8950cad0f8293121f651a74de94a6f3c
MgTintWai <tintwai544@gmail.com>
2023-11-20 11:39:35 +0630

    update commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Link href="/hello">Main Page</Link>
+    <Link href="/">Show Page</Link>
+    <div>The page with time {{ timer }}</div>
+    <slot>Default</slot>
+  </div>
+</template>
+
+<script setup>
+
+import { Link } from '@inertiajs/vue3'
+// import { Link } from '@inertiajs/inertia-vue3'
+import { ref } from 'vue'
+
+const timer = ref(0)
+
+setInterval(() => timer.value++, 1000)
+
+</script>
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <MainLayout> -->
+  <!-- Pascal Case -->
+  <div>Index</div>
+  <div>
+    This is {{ message }}
+  </div>
+  <!-- </MainLayout> -->
+</template>
+
+<script setup>
+
+defineProps({
+  message : String,
+})
+
+</script>
+<!-- 
+<script>
+
+import MainLayout from '../../Layouts/MainLayout.vue'
+export default {
+    layout: MainLayout
+}
+</script> -->
