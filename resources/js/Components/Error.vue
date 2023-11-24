6547a21b4a5d1d82a425a14a4b0d4af27bcf3b9c
MgTintWai <tintwai544@gmail.com>
2023-11-24 15:17:04 +0630

    update payment

---
+++ resources/js/Components/Error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="form" class="input-error">
+    {{ form }}
+  </div>
+</template>
+
+<script setup>
+
+defineProps({
+  form:String,
+})
+
+</script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,7 +3,15 @@
     <Box v-for="listing in listings" :key="listing.id">
       <div>
         <Link :href="route('listing.show',{listing:listing.id})">
-          <Price :price="listing.price" class="text-2xl font-bold" />
+          <div class="flex items-center gap-1">
+            <Price
+              :price="listing.price"
+              class="text-2xl font-bold"
+            />
+            <div class="text-xs text-gray-500">
+              <Price :price="monthlyPayment" /> pm
+            </div>
+          </div>>
           <ListingSpace :listing="listing" class="text-lg" />
           <LinkAddress :listing="listing" class="text-gray-500" />
         </Link>
